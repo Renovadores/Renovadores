9915b33b165c2f0e1f23a13e281ea86543f806c0
kvn2912 <kevinjbr29@gmail.com>
2023-04-30 14:48:52 -0600

    Se añadió formulario para agregar clientes y espacio para buscar clientes.

--- FicusApp/ClientApp/src/components/Clients.js
+++ FicusApp/ClientApp/src/components/Clients.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react'
 
 function Clients() {
   // TO-DO fetch data from back-end
@@ -40,56 +41,222 @@ function Clients() {
   ]
 
   // When user click on client button, 'navigate hook' redirect him to new page
-  const navigate = useNavigate(); // Allows referencinging a specific path defined in AppRoutes
+  const navigate = useNavigate(); // Allows referencing a specific path defined in AppRoutes
   const handleClick = (clientId, clientName) => {
     navigate('/clientes/informacion', { state: { id: clientId, name: clientName } });
     //second argument allows to pass parameters
   };
+
+  const [company, setCompany] = useState("");
+  const handleChangeCompany = (event) => {
+    setCompany(event.target.value)
+  }
+
+  useEffect(() => {
+    console.log(company);
+    }, [company]);
+
+  const handleCancel = () => {
+    setCompany("");
+    // TO-DO: clear info for the other fields 
+  }
+
+  const handleSubmit = (event) => {
+    
+  }
+
+  const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+
   // TO-DO: separate in new components to simplify code
   return (
     <div className="container pt-3">
       <div className="row m-2 mb-4">
-        <div className="col-3">
-          <h4>Clientes</h4>
-        </div>
-        <div className="col-3 d-flex justify-content-end">
+        <h4>Clientes</h4>
+      </div>
+      <div className="row m-2 mb-5">
+        <div className="col-sm-6 col-md-3 d-flex my-2 my-md-0">
           <button className="btn btn-success" type="button" data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Agregar Cliente</button>
-
+            data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+            Agregar Cliente
+          </button>
           <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
             <div className="offcanvas-header">
               <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Informacion del nuevo cliente</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={handleCancel}></button>
             </div>
             <div className="offcanvas-body">
-              <p>Formulario</p>
-              {/* TO-DO: Add form */}
-              <button className="btn btn-success">Agregar</button>
+              <form onSubmit={handleSubmit}>
+
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" value={company} onChange={handleChangeCompany} />
+                  <label htmlFor="floatingInput">Empresa</label>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="formGroupExampleInput" className="form-label">Agregado el: {date}</label>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="formGroupExampleInput" className="form-label">Segmento</label>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Cafeteria</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Catering</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Centro educativo</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Comida preparada</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Empresa</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Feria</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Otro Sector</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Panaderia</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Restaurante</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Usuario Final</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Supermercado</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Otro</label>
+                  </div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="formGroupExampleInput" className="form-label">Responsable</label>
+                  <select className="form-select" aria-label="Default select example">
+                    <option value="1">Alejandro</option>
+                    <option value="2">Andrea</option>
+                    <option value="3">Fabiola</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="formGroupExampleInput" className="form-label">Prioridad</label>
+                  <select className="form-select" aria-label="Default select example">
+                    <option value="1">Baja</option>
+                    <option value="2">Media</option>
+                    <option value="3">Alta</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="formGroupExampleInput" className="form-label">Estado</label>
+                  <select className="form-select" aria-label="Default select example">
+                    <option value="1">Clientes</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="formGroupExampleInput" className="form-label">Medio de Comunicacion</label>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Correo</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Llamada</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Instagram</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Whatsapp</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Zoom</label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">Otra</label>
+                  </div>
+                </div>
+
+
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                  <label htmlFor="floatingInput">Contacto</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                  <input type="tel" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                  <label htmlFor="floatingInput">Telefono</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                  <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                  <label htmlFor="floatingInput">Correo Electronico</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                  <label htmlFor="floatingInput">Pagina Web</label>
+                </div>
+                <div className="row">
+                  <div className="col-6 d-flex justify-content-center">
+                    <button type="submit" className="btn btn-primary">Agregar</button>
+                  </div>
+                  <div className="col-6 d-flex justify-content-center">
+                    <button type="submit" className="btn btn-danger">Cancelar</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-
         </div>
-        <div className="col-3 d-flex justify-content-begin">
+        <div className="col-sm-6 col-md-3  d-flex my-2 my-md-0">
           <button className="btn btn-danger">Eliminar Cliente</button>
         </div>
-        <div className="col-3">
-          <div className="dropdown d-flex justify-content-end">
+        <div className="col-sm-6 col-md-3 d-flex my-2 my-md-0">
+          <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Buscar cliente..." />
+        </div>
+
+        <div className="col-sm-6 col-md-3 d-flex my-2 my-md-0">
+          <div className="dropdown">
             <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Filtrar
+              Filtrado
             </button>
             <ul className="dropdown-menu">
               <li><a className="dropdown-item" href="#">Prioridad</a></li>
-              <li><a className="dropdown-item" href="#">Fecha</a></li>
-              <li><a className="dropdown-item" href="#">Reservaciones</a></li>
+              <li><a className="dropdown-item" href="#">Recientes</a></li>
             </ul>
           </div>
         </div>
       </div>
-
       <div className="row m-2 mt-4 d-flex justify-content-center">      
           {
             clients.map((client) => (
-              <div className="col-sm-6 col-md-3" key={client.id}>
+              <div className="col-sm-6 col-md-3 mb-3" key={client.id}>
                 <div className="card">
                   <div className="card-body">
                     <h5 className="card-title">{client.name}</h5>
@@ -106,15 +273,15 @@ function Clients() {
         <nav aria-label="...">
           <ul className="pagination justify-content-center">
             <li className="page-item disabled">
-              <a className="page-link">Previous</a>
+              <a className="page-link">Anterior</a>
             </li>
-            <li className="page-item"><a className="page-link" href="#">1</a></li>
-            <li className="page-item active" aria-current="page">
+            <li className="page-item active"><a className="page-link" href="#">1</a></li>
+            <li className="page-item" aria-current="page">
               <a className="page-link" href="#">2</a>
             </li>
             <li className="page-item"><a className="page-link" href="#">3</a></li>
             <li className="page-item">
-              <a className="page-link" href="#">Next</a>
+              <a className="page-link" href="#">Siguiente</a>
             </li>
           </ul>
         </nav>
