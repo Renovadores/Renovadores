551fce881f60d533e5bb65cc9814862d9132fea2
isabe <isabelafrr06@hotmail.com>
2023-05-01 13:28:08 -0600

    Bootstrap default theme change

--- FicusApp/ClientApp/src/components/NavMenu.js
+++ FicusApp/ClientApp/src/components/NavMenu.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
 
 function NavMenu() {
   const [collapsed, setState] = useState(true);
@@ -10,22 +16,45 @@ function NavMenu() {
   };
   return (
     <header>
-      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-        <NavbarBrand tag={Link} to="/">FicusApp</NavbarBrand>
+      <Navbar
+        className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+        color="primary"
+      >
+        <img
+          src="images\marca\JPG\Logotipo (7).jpg"
+          alt="logo"
+          width="30"
+          height="30"
+        />
+        <NavbarBrand tag={Link} className="text-light" to="/">
+          FicusApp
+        </NavbarBrand>
         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+        <Collapse
+          className="d-sm-inline-flex flex-sm-row-reverse"
+          isOpen={!collapsed}
+          navbar
+        >
           <ul className="navbar-nav flex-grow">
             <NavItem>
-              <NavLink tag={Link} className="text-dark" to="/">Inicio</NavLink>
+              <NavLink tag={Link} className="text-light" to="/">
+                Inicio
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={Link} className="text-dark" to="/clientes">Clientes</NavLink>
+              <NavLink tag={Link} className="text-light" to="/clientes">
+                Clientes
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={Link} className="text-dark" to="/inventario">Inventario</NavLink>
+              <NavLink tag={Link} className="text-light" to="/inventario">
+                Inventario
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={Link} className="text-dark" to="/cerrarSesion">Cerrar Sesion</NavLink>
+              <NavLink tag={Link} className="text-light" to="/cerrarSesion">
+                Cerrar Sesion
+              </NavLink>
             </NavItem>
           </ul>
         </Collapse>
--- FicusApp/ClientApp/src/components/Stock.js
+++ FicusApp/ClientApp/src/components/Stock.js
@@ -5,17 +5,6 @@ function Stock() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Inventario</title>
-
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-          crossOrigin="anonymous"
-        />
-
-        <link href="css/bootstrap-override.css" rel="stylesheet" />
-
-        <link href="css/custom.css" rel="stylesheet" />
       </head>
       <body>
         <main role="main">
@@ -24,7 +13,7 @@ function Stock() {
               <h1 className="display-3 fw-bold">Nuestros productos</h1>
             </div>
             <div className="d-grid gap-2 mb-4">
-              <button className="btn btn-primary" type="button">
+              <button className="btn btn-success" type="button">
                 Agregar Producto
               </button>
             </div>
