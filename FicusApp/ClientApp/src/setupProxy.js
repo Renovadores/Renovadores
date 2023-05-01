d134d583b67479f647ba972ecbedfd7d185de67f
kvn2912 <kevinjbr29@gmail.com>
2023-05-01 15:01:30 -0600

    Implementación del backend para obtener los clientes en una base de datos

--- FicusApp/ClientApp/src/setupProxy.js
+++ FicusApp/ClientApp/src/setupProxy.js
@@ -5,7 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:17998';
 
 const context = [
-  "/weatherforecast",
+  "/api/cliente",
 ];
 
 const onError = (err, req, resp, target) => {
---
+++ FicusApp/Models/FicusDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FicusApp.Models;
+
+public partial class FicusDbContext : DbContext
+{
+    public FicusDbContext()
+    {
+    }
+
+    public FicusDbContext(DbContextOptions<FicusDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=KEVIN\\BD_KEVIN; DataBase=FicusDB; Integrated Security=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Agregado).HasColumnType("date");
+            entity.Property(e => e.Empresa).HasMaxLength(50);
+            entity.Property(e => e.Prioridad).HasMaxLength(50);
+            entity.Property(e => e.Responsable).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
