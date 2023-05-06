42a32242fd3d54c0d2bdb945cb7be9d41f87ecc2
isabe <isabelafrr06@hotmail.com>
2023-05-05 21:52:24 -0600

    Conexión de clientes con base de datos

---
+++ FicusApp/ClientApp/src/components/ClientInformation.js
@@ -0,0 +1,15 @@
+import { useLocation } from 'react-router-dom';
+
+function ClientInformation() {
+  const location = useLocation();
+  // get id and client name sent by navigate hook in Client.js
+  let clientId = location.state.id;
+  let clientName = location.state.name;
+  return (
+    <div className="container">
+      <h1>Empresa: {clientName}</h1>
+    </div>
+  );
+}
+
+export default ClientInformation;
