b7011dab6db749a5f0d985420f113ca66f74d48e
Oscar Quesada Webb <oskar.quesada30@gmail.com>
2023-05-07 12:00:07 -0600

    Agregar script instalacion conversor DMBL a SQL

--- DataBase/get_dotsql.sh
+++ DataBase/get_dotsql.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
+if [ "$(npm list -g --depth=0 | grep -c @dbml/cli)" -eq 0 ]; then
+  echo "Instalando dependencia.."
+  npm install -g @dbml/cli
+fi
+
 dbml2sql ./ficus.dbml --mssql -o ./db_ficus.sql
