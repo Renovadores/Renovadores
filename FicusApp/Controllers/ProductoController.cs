9674b8d7455e9e4113fa5e0168d4cfb2fe3d716d
JAVIER.DONATO <javier.donato@ucr.ac.cr>
2023-05-09 15:35:50 -0800

    update

---
+++ FicusApp/Controllers/ProductoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FicusApp.Models;
+
+namespace FicusApp.Controllers
+{
+   [Route("api/[controller]")]
+    [ApiController]
+public class ProductoController : ControllerBase
+    {
+        private readonly FicusDbContext _context;
+
+        public ProductoController(FicusDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetProducts")]
+        public async Task<IActionResult> GetProducts()
+        {
+            List<Producto> productos = _context.Producto.OrderByDescending(c => c.ProductoID).ToList();
+            return Ok(productos);
+        }
+
+        [HttpGet]
+        [Route("GetProducto/{id}")]
+        public async Task<IActionResult> GetProducto(int id)
+        {
+            Producto producto = await _context.Producto.FindAsync(id);
+            return Ok(producto);
+        }
+
+        [HttpPost]
+        [Route("AddProduct")]
+        public async Task<IActionResult> AddProduct([FromBody] Producto request)
+        {
+            await _context.Productos.AddAsync(request);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
+
