e45581caa9c4ab0e08c668ec754ee3af054437d4
BryanVillegas <bryan.villegasalvarado@ucr.ac.cr>
2023-05-09 23:11:44 -0600

    login frontend

---
+++ FicusApp/ClientApp/src/components/Login.js
@@ -0,0 +1,25 @@
+﻿import React, { useState } from "react"
+
+function Login(props) {
+    const [username, setUsername] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="Username">Nombre de usuario</label>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} type="username" id="Username" placeholder="Usuario"></input>
+                <label htmlFor="Password">Contraseña</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="Password" placeholder="******"></input>
+
+                <button type="submit">Iniciar sesión</button>
+            </form>
+        </div>
+    );
+}
+export default Login;
