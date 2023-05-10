193b88aa723c05e9dbbcdbb9e504592183224b00
JAVIER.DONATO <javier.donato@ucr.ac.cr>
2023-05-09 16:39:11 -0800

    AddProduct UI

--- FicusApp/ClientApp/src/components/Stock.js
+++ FicusApp/ClientApp/src/components/Stock.js
@@ -98,7 +98,7 @@ function Stock() {
     const date = `${current.getDate()}-${current.getMonth() + 1}-${current.getFullYear()}`; 
 
   return (
-    <><div>
+      <div>
           <head>
               <meta charSet="utf-8" />
               <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -125,7 +125,8 @@ function Stock() {
                                   className="form-control"
                                   list="datalistOptions"
                                   id="exampleDataList"
-                                  placeholder="Buscar producto..." />
+                                  placeholder="Buscar producto..."
+                              />
                               {/* Filter/Search button*/}
                               <div className="col-sm-6 col-md-3 d-flex my-2 my-md-0 ms-2">
                                   <div className="dropdown">
@@ -152,153 +153,160 @@ function Stock() {
                                   </div>
                               </div>
                           </div>
-                          <button className="btn btn-success" type="button" data-bs-toggle="offcanvas"
-                              data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
-                              Agregar Producto
+                          <div className="d-grid gap-2 mb-4">
+                              <button className="btn btn-success" type="button" data-bs-toggle="offcanvas"
+                                  data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+                                  Agregar Producto
+                              </button>
+                          </div>
+                      </div>
+                      <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+                          <div className="offcanvas-header">
+                              <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Informacion del nuevo producto</h5>
+                              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                          </div>
+                          <div className="offcanvas-body">
+                              <form onSubmit={handleSubmit}>
+                                  <input variable={name} handler={handleChangeName} text="Nombre" />
+                                  <div className="mb-3">
+                                      <label htmlFor="formGroupExampleInput" className="form-label">Agregado el: {date}</label>
+                                  </div>
+                                  <input variable={descripcion} handler={handleChangeDescripcion} text="Descripcion" />
+                                  <input variable={dimensiones} handler={handleChangeDimensiones} text="Dimensiones" />
+                                  <input variable={peso_recipiente} handler={handleChangePeso_recipiente} text="Peso del Recipiente" />
+                                  <input variable={peso_desechable} handler={handleChangePeso_desechable} text="Peso Desechable" />
+                                  <input variable={alquiler_comercios} handler={handleChangeAlquiler_Comercios} text="Alquiler comercios" />
+                                  <input variable={alquiler_retail} handler={handleChangeAlquiler_Retail} text="Alquiler Retail" />
+
+                                  <div className="row">
+                                      <div className="col-6 d-flex justify-content-center">
+                                          <button type="submit" className="btn btn-primary" data-bs-dismiss="offcanvas" >Agregar</button>
+                                      </div>
+                                      <div className="col-6 d-flex justify-content-center">
+                                          <button className="btn btn-danger" type="button" onClick={handleCancel} data-bs-dismiss="offcanvas">Cancelar</button>
+                                      </div>
+                                  </div>
+
+                              </form>
+                          </div>
+                      </div>
+                          <button className="btn btn-warning" type="button">
+                              Eliminar Producto
                           </button>
                       </div>
-                  </div>
-                  <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-                      <div className="offcanvas-header">
-                          <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Informacion del nuevo producto</h5>
-                          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                      </div>
-                      <div className="offcanvas-body">
-                          <form onSubmit={handleSubmit}>
-                              <input variable={name} handler={handleChangeName} text="Nombre" />
-                              <div className="mb-3">
-                                  <label htmlFor="formGroupExampleInput" className="form-label">Agregado el: {date}</label>
-                              </div>
-                              <input variable={descripcion} handler={handleChangeDescripcion} text="Descripcion" />
-                              <input variable={dimensiones} handler={handleChangeDimensiones} text="Dimensiones" />
-                              <input variable={peso_recipiente} handler={handleChangePeso_recipiente} text="Peso del Recipiente" />
-                              <input variable={peso_desechable} handler={handleChangePeso_desechable} text="Peso Desechable" />
-                              <input variable={alquiler_comercios} handler={handleChangeAlquiler_Comercios} text="Alquiler comercios" />
-                              <input variable={alquiler_retail} handler={handleChangeAlquiler_Retail} text="Alquiler Retail" />
-
-                              <div className="row">
-                                  <div className="col-6 d-flex justify-content-center">
-                                      <button type="submit" className="btn btn-primary" data-bs-dismiss="offcanvas">Agregar</button>
+              </section>
+              <section className="py-4">
+                  <div className="container-fluid">
+                      <div className="row">
+                          {/* Vajilla reusable card*/}
+                          <div className="col-sm-6 col-lg-4 mb-4">
+                              <div className="card text-center">
+                                  <img
+                                      className="card-img-top"
+                                      src="images\productos\M-275-BL  (2).png"
+                                      alt="Producto Ficus"
+                                  />
+                                  <div className="card-body">
+                                      <h5 className="card-title">Vajilla Reusable</h5>
+                                      <p className="card-text">
+                                          Variedad de vajillas 100% retornables y reciclables para
+                                          mejorar la experiencia de tus clientes
+                                      </p>
                                   </div>
-                                  <div className="col-6 d-flex justify-content-center">
-                                      <button className="btn btn-danger" type="button" onClick={handleCancel} data-bs-dismiss="offcanvas">Cancelar</button>
+                                  <div className="card-footer">
+                                      <a href="index.html" className="btn btn-primary">
+                                          Ver Todos
+                                      </a>
                                   </div>
                               </div>
-
-                          </form>
+                          </div>
+                          {/* Recipientes para llevar card*/}
+                          <div className="col-sm-6 col-lg-4 mb-4">
+                              <div className="card text-center">
+                                  <img
+                                      className="card-img-top"
+                                      src="images\productos\EC-07-1-JA Montezuma (1).png"
+                                      alt="Producto Ficus"
+                                  />
+                                  <div className="card-body">
+                                      <h5 className="card-title">Recipientes para llevar</h5>
+                                      <p className="card-text">
+                                          Recipientes para llevar, reciclables, retornables, aptos
+                                          para microondas y mucho más
+                                      </p>
+                                  </div>
+                                  <div className="card-footer">
+                                      <a href="index.html" className="btn btn-primary">
+                                          Ver Todos
+                                      </a>
+                                  </div>
+                              </div>
+                          </div>
                       </div>
                   </div>
+              </section>
+              <section className="py-4">
+                  <div className="container fluid">
+                      <div className="container">
+                          <h2 className="display-3 fw-bold">Todos los productos</h2>
+                      </div>
+                      <div className="row">
 
-                  <button className="btn btn-warning" type="button">
-                      Eliminar Producto
-                  </button>
+                          {/*Database card list */}
+                          {products.map((product, productIndex) => (
+                              <div
+                                  className="col-sm-6 col-md-3 mb-3"
+                                  key={product.productoId}
+                              >
+                                  <div className="card">
+                                      <div className="card-body">
+                                          <h5 className="card-title">{product.nombre}</h5>
+                                          <p className="card-text">{product.descripcion}</p>
+                                          <button
+                                              className="btn btn-primary"
+                                              onClick={() =>
+                                                  handleClickViewProduct(productIndex)
+                                              }
+                                          > Ver producto
+                                          </button>
+                                      </div>
+                                  </div>
+                              </div>
+                          ))}
+                      </div>
+                      <div className="row m-2 mt-4">
+                          <nav aria-label="...">
+                              <ul className="pagination justify-content-center">
+                                  <li className="page-item disabled">
+                                      <a className="page-link">Anterior</a>
+                                  </li>
+                                  <li className="page-item active">
+                                      <a className="page-link" href="#">
+                                          1
+                                      </a>
+                                  </li>
+                                  <li className="page-item" aria-current="page">
+                                      <a className="page-link" href="#">
+                                          2
+                                      </a>
+                                  </li>
+                                  <li className="page-item">
+                                      <a className="page-link" href="#">
+                                          3
+                                      </a>
+                                  </li>
+                                  <li className="page-item">
+                                      <a className="page-link" href="#">
+                                          Siguiente
+                                      </a>
+                                  </li>
+                              </ul>
+                          </nav>
+                      </div>
+                  </div>
               </section>
           </body>
-      </div><section className="py-4">
-              <div className="container-fluid">
-                  <div className="row">
-                      {/* Vajilla reusable card*/}
-                      <div className="col-sm-6 col-lg-4 mb-4">
-                          <div className="card text-center">
-                              <img
-                                  className="card-img-top"
-                                  src="images\productos\M-275-BL  (2).png"
-                                  alt="Producto Ficus" />
-                              <div className="card-body">
-                                  <h5 className="card-title">Vajilla Reusable</h5>
-                                  <p className="card-text">
-                                      Variedad de vajillas 100% retornables y reciclables para
-                                      mejorar la experiencia de tus clientes
-                                  </p>
-                              </div>
-                              <div className="card-footer">
-                                  <a href="index.html" className="btn btn-primary">
-                                      Ver Todos
-                                  </a>
-                              </div>
-                          </div>
-                      </div>
-                      {/* Recipientes para llevar card*/}
-                      <div className="col-sm-6 col-lg-4 mb-4">
-                          <div className="card text-center">
-                              <img
-                                  className="card-img-top"
-                                  src="images\productos\EC-07-1-JA Montezuma (1).png"
-                                  alt="Producto Ficus" />
-                              <div className="card-body">
-                                  <h5 className="card-title">Recipientes para llevar</h5>
-                                  <p className="card-text">
-                                      Recipientes para llevar, reciclables, retornables, aptos
-                                      para microondas y mucho más
-                                  </p>
-                              </div>
-                              <div className="card-footer">
-                                  <a href="index.html" className="btn btn-primary">
-                                      Ver Todos
-                                  </a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section><section className="py-4">
-              <div className="container fluid">
-                  <div className="container">
-                      <h2 className="display-3 fw-bold">Todos los productos</h2>
-                  </div>
-                  <div className="row">
-
-                      {/*Database card list */}
-                      {products.map((product, productIndex) => (
-                          <div
-                              className="col-sm-6 col-md-3 mb-3"
-                              key={product.productoId}
-                          >
-                              <div className="card">
-                                  <div className="card-body">
-                                      <h5 className="card-title">{product.nombre}</h5>
-                                      <p className="card-text">{product.descripcion}</p>
-                                      <button
-                                          className="btn btn-primary"
-                                          onClick={() => handleClickViewProduct(productIndex)}
-                                      > Ver producto
-                                      </button>
-                                  </div>
-                              </div>
-                          </div>
-                      ))}
-                  </div>
-                  <div className="row m-2 mt-4">
-                      <nav aria-label="...">
-                          <ul className="pagination justify-content-center">
-                              <li className="page-item disabled">
-                                  <a className="page-link">Anterior</a>
-                              </li>
-                              <li className="page-item active">
-                                  <a className="page-link" href="#">
-                                      1
-                                  </a>
-                              </li>
-                              <li className="page-item" aria-current="page">
-                                  <a className="page-link" href="#">
-                                      2
-                                  </a>
-                              </li>
-                              <li className="page-item">
-                                  <a className="page-link" href="#">
-                                      3
-                                  </a>
-                              </li>
-                              <li className="page-item">
-                                  <a className="page-link" href="#">
-                                      Siguiente
-                                  </a>
-                              </li>
-                          </ul>
-                      </nav>
-                  </div>
-              </div>
-          </section></>
+      </div>
   );
 }
 
