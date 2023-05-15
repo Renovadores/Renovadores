450f08e79e4e5e8c2efd772f63aed02fc718bfc3
Oscar Quesada Webb <oskar.quesada30@gmail.com>
2023-05-14 20:52:44 -0600

    Agregar plantilla Horizon UI

---
+++ FicusApp/ClientApp/src/components/Vacio.js
@@ -0,0 +1,14 @@
+// Chakra imports
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
+
+const Vacio = () => {
+  // Chakra Color Mode
+  return (
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <h1> Aquí van las cosas </h1>
+    </Box>
+  );
+}
+
+export default Vacio;
