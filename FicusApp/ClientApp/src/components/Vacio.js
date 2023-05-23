06f78fad3c4bbe470b644589d84326c5f5670a94
Oscar Quesada Webb <oskar.quesada30@gmail.com>
2023-05-23 16:00:39 -0600

    Agregar archivos para HorizonUI adaptado a Ficus

---
+++ FicusApp/ClientApp/src/components/Vacio.js
@@ -0,0 +1,14 @@
+// Chakra imports
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
+
+const Vacio = () => {
+  // Chakra Color Mode
+  return (
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <p> Aquí van las cosas </p>
+    </Box>
+  );
+}
+
+export default Vacio;
---
+++ FicusApp/ClientApp/src/components/sidebar/components/Content.js
@@ -0,0 +1,32 @@
+import logo_sidebar from "../../../assets/ficus/img/tagline_red.png"
+// chakra imports
+import { Box, Flex, Stack, Center, Image } from "@chakra-ui/react";
+//   Custom components
+import Links from "components/sidebar/components/Links";
+import { HSeparator } from "components/separator/Separator";
+import React from "react";
+
+// FUNCTIONS
+
+function SidebarContent(props) {
+  const { routes } = props;
+  // SIDEBAR
+  return (
+    <Flex direction='column' height='100%' pt='25px' borderRadius='30px'>
+      <Flex align='center' direction='column' >
+        <Image src={logo_sidebar} alt="Ficus logo"
+          style={{ 
+            marginTop: "-30%", 
+            marginBottom: "-20%" }}
+        />
+      </Flex>
+      <Stack direction='column' mb='auto' mt='8px'>
+        <Box ps='20px' pe={{ md: "16px", "2xl": "1px" }}>
+          <Links routes={routes} />
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
+
+export default SidebarContent;
