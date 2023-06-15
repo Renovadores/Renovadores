d11ccb4c6226af051fa432078b2a1950c7ea4cfb
isabe <isabelafrr06@hotmail.com>
2023-06-14 20:05:04 -0600

    corregidos errores lint

--- FicusApp/ClientApp/src/components/Stock.js
+++ FicusApp/ClientApp/src/components/Stock.js
@@ -1,10 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Input from "./Input";
 import InputInt from "./InputInt";
-import CheckBox from "./CheckBox";
 import SelectColor from "./SelectColor";
 import SelectCategory from "./SelectCategory";
 import SelectFamily from "./SelectFamily";
@@ -95,29 +93,29 @@ function Stock() {
     setColorId(event.target.value);
   };
   const [categoriaId, setCategoriaId] = useState(1);
-    const handleChangeCategoriaId = (event) => {
-        setCategoriaId(event.target.value);
-    };
-    const [descontinuado, setDescontinuado] = useState(1);
-    const handleChangeDescontinuado = (event) => {
-        setDescontinuado(event.target.value);
-    };
-    const [totalExistente, setTotalExistente] = useState(1);
-    const handleChangeTotalExistente = (event) => {
-        setTotalExistente(event.target.value);
-    };
-    const [enUso, setEnUso] = useState(1);
-    const handleChangeEnUso = (event) => {
-        setEnUso(event.target.value);
-    };
-    const [disponible, setDisponible] = useState(1);
-    const handleChangeDisponible = (event) => {
-        setDisponible(event.target.value);
-    };
-    const [noDevueltos, setNoDevueltos] = useState(1);
-    const handleChangeNoDevueltos = (event) => {
-        setNoDevueltos(event.target.value);
-    };
+  const handleChangeCategoriaId = (event) => {
+    setCategoriaId(event.target.value);
+  };
+  /* TO DO const [descontinuado, setDescontinuado] = useState(0);
+  const handleChangeDescontinuado = (event) => {
+    setDescontinuado(event.target.value);
+  };*/
+  const [totalExistente, setTotalExistente] = useState(1);
+  const handleChangeTotalExistente = (event) => {
+    setTotalExistente(event.target.value);
+  };
+  const [enUso, setEnUso] = useState(1);
+  const handleChangeEnUso = (event) => {
+    setEnUso(event.target.value);
+  };
+  const [disponible, setDisponible] = useState(1);
+  const handleChangeDisponible = (event) => {
+    setDisponible(event.target.value);
+  };
+  const [noDevueltos, setNoDevueltos] = useState(1);
+  const handleChangeNoDevueltos = (event) => {
+    setNoDevueltos(event.target.value);
+  };
 
   const handleCancel = () => {
     setProductoId("");
@@ -131,7 +129,7 @@ function Stock() {
     setColorId(0);
     setCategoriaId(0);
     setFamiliaId(0);
-    setDescontinuado(0);
+    //setDescontinuado(0);
     setTotalExistente(0);
     setEnUso(0);
     setDisponible(0);
@@ -141,22 +139,22 @@ function Stock() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log(
-        date,
-        productoId,
-        nombre,
-        colorId,
-        descripcion,
-        dimensiones,
-        pesoRecipiente,
-        pesoDesechable,
-        alquilerComercios,
-        alquilerRetail,
-        categoriaId,
-        familiaId,
-        totalExistente,
-        enUso, 
-        disponible,
-        noDevueltos
+      date,
+      productoId,
+      nombre,
+      colorId,
+      descripcion,
+      dimensiones,
+      pesoRecipiente,
+      pesoDesechable,
+      alquilerComercios,
+      alquilerRetail,
+      categoriaId,
+      familiaId,
+      totalExistente,
+      enUso,
+      disponible,
+      noDevueltos
     );
     const responseProduct = await fetch("api/producto/AddProduct", {
       method: "POST",
@@ -197,97 +195,97 @@ function Stock() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Inventario</title>
       </head>
-        <section className="py-4">
-          <div className="container-fluid">
+      <section className="py-4">
+        <div className="container-fluid">
+          <div className="d-grid gap-2 mb-4">
             <div className="d-grid gap-2 mb-4">
-              <div className="d-grid gap-2 mb-4">
-                <div className="row">
-                    <div className="col-6 d-flex justify-content-start">
-                        <button
-                            className="btn btn-success"
-                            type="button"
-                            data-bs-toggle="offcanvas"
-                            data-bs-target="#offcanvasWithBothOptions"
-                            aria-controls="offcanvasWithBothOptions"
-                        >
-                            Agregar Producto
-                        </button>
-                    </div>
+              <div className="row">
+                <div className="col-6 d-flex justify-content-start">
+                  <button
+                    className="btn btn-success"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasWithBothOptions"
+                    aria-controls="offcanvasWithBothOptions"
+                  >
+                    Agregar Producto
+                  </button>
                 </div>
-
               </div>
             </div>
-            <div
-              className="offcanvas offcanvas-start"
-              data-bs-scroll="true"
-              tabIndex="-1"
-              id="offcanvasWithBothOptions"
-              aria-labelledby="offcanvasWithBothOptionsLabel"
-            >
-              <div className="offcanvas-header">
-                <h5
-                  className="offcanvas-title"
-                  id="offcanvasWithBothOptionsLabel"
-                >
-                  Informacion del nuevo producto
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="offcanvas"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="offcanvas-body">
-                <form onSubmit={handleSubmit}>
-                  <Input variable={productoId} handler={handleChangeProductoId} text="Producto ID" />
-                  <Input
-                    variable={nombre}
-                    handler={handleChangeNombre}
-                    text="Nombre"
-                  />
-                  <div className="mb-3">
-                    <label
-                      htmlFor="formGroupExampleInput"
-                      className="form-label"
-                    >
-                      Agregado el: {date}
-                    </label>
-                  </div>
-                  <Input
-                    variable={descripcion}
-                    handler={handleChangeDescripcion}
-                    text="Descripción"
-                  />
-                  <Input
-                    variable={dimensiones}
-                    handler={handleChangeDimensiones}
-                    text="Dimensiones"
-                  />
-                  <InputInt
-                    variable={pesoRecipiente}
-                    handler={handleChangePesoRecipiente}
-                    text="Peso de Recipiente"
-                  />
-                  <InputInt
-                    variable={pesoDesechable}
-                    handler={handleChangePesoDesechable}
-                    text="Peso Desechable"
-                  />
-                  <InputInt
-                    variable={alquilerComercios}
-                    handler={handleChangeAlquilerComercios}
-                    text="Precio Comercio"
-                  />
-                  <InputInt
-                    variable={alquilerRetail}
-                    handler={handleChangeAlquilerRetail}
-                    text="Precio Retail"
-                                  />
-                 <InputInt
-                    variable={totalExistente}
-                    handler={handleChangeTotalExistente}
-                    text="Total de Productos Existentes"
+          </div>
+          <div
+            className="offcanvas offcanvas-start"
+            data-bs-scroll="true"
+            tabIndex="-1"
+            id="offcanvasWithBothOptions"
+            aria-labelledby="offcanvasWithBothOptionsLabel"
+          >
+            <div className="offcanvas-header">
+              <h5
+                className="offcanvas-title"
+                id="offcanvasWithBothOptionsLabel"
+              >
+                Informacion del nuevo producto
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <form onSubmit={handleSubmit}>
+                <Input
+                  variable={productoId}
+                  handler={handleChangeProductoId}
+                  text="Producto ID"
+                />
+                <Input
+                  variable={nombre}
+                  handler={handleChangeNombre}
+                  text="Nombre"
+                />
+                <div className="mb-3">
+                  <label htmlFor="formGroupExampleInput" className="form-label">
+                    Agregado el: {date}
+                  </label>
+                </div>
+                <Input
+                  variable={descripcion}
+                  handler={handleChangeDescripcion}
+                  text="Descripción"
+                />
+                <Input
+                  variable={dimensiones}
+                  handler={handleChangeDimensiones}
+                  text="Dimensiones"
+                />
+                <InputInt
+                  variable={pesoRecipiente}
+                  handler={handleChangePesoRecipiente}
+                  text="Peso de Recipiente"
+                />
+                <InputInt
+                  variable={pesoDesechable}
+                  handler={handleChangePesoDesechable}
+                  text="Peso Desechable"
+                />
+                <InputInt
+                  variable={alquilerComercios}
+                  handler={handleChangeAlquilerComercios}
+                  text="Precio Comercio"
+                />
+                <InputInt
+                  variable={alquilerRetail}
+                  handler={handleChangeAlquilerRetail}
+                  text="Precio Retail"
+                />
+                <InputInt
+                  variable={totalExistente}
+                  handler={handleChangeTotalExistente}
+                  text="Total de Productos Existentes"
                 />
                 <InputInt
                   variable={enUso}
@@ -305,16 +303,15 @@ function Stock() {
                   text="Productos No Devueltos"
                 />
 
-
-                  <SelectCategory
-                    variable={categoriaId}
-                    handler={handleChangeCategoriaId}
-                  />
-                  <SelectFamily
-                    variable={familiaId}
-                    handler={handleChangeFamiliaId}
-                  />
-                  <SelectColor variable={colorId} handler={handleChangeColorId} />
+                <SelectCategory
+                  variable={categoriaId}
+                  handler={handleChangeCategoriaId}
+                />
+                <SelectFamily
+                  variable={familiaId}
+                  handler={handleChangeFamiliaId}
+                />
+                <SelectColor variable={colorId} handler={handleChangeColorId} />
 
                 <div className="row">
                   <div className="col-6 d-flex justify-content-center">
@@ -363,12 +360,12 @@ function Stock() {
               </button>
               <ul className="dropdown-menu">
                 <li>
-                  <a className="dropdown-item" href="#">
+                  <a className="dropdown-item" href="/productos/informacion/">
                     Prioridad
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="#">
+                  <a className="dropdown-item" href="/productos/informacion/">
                     Recientes
                   </a>
                 </li>
@@ -402,25 +399,27 @@ function Stock() {
             <nav aria-label="...">
               <ul className="pagination justify-content-center">
                 <li className="page-item disabled">
-                  <a className="page-link">Anterior</a>
+                  <a className="page-link" href="/productos/informacion/">
+                    Anterior
+                  </a>
                 </li>
                 <li className="page-item active">
-                  <a className="page-link" href="#">
+                  <a className="page-link" href="/productos/informacion/">
                     1
                   </a>
                 </li>
                 <li className="page-item" aria-current="page">
-                  <a className="page-link" href="#">
+                  <a className="page-link" href="/productos/informacion/">
                     2
                   </a>
                 </li>
                 <li className="page-item">
-                  <a className="page-link" href="#">
+                  <a className="page-link" href="/productos/informacion/">
                     3
                   </a>
                 </li>
                 <li className="page-item">
-                  <a className="page-link" href="#">
+                  <a className="page-link" href="/productos/informacion/">
                     Siguiente
                   </a>
                 </li>
@@ -429,7 +428,7 @@ function Stock() {
           </div>
         </div>
       </section>
-      </div>
+    </div>
   );
 }
 
