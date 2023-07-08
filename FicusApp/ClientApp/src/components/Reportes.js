74c6d6dc00e956ba896a3ce8a69449f0682fbad1
JAVIER.DONATO <javier.donato@ucr.ac.cr>
2023-07-07 18:47:25 -0800

    Reporte Huella Ambiental

--- FicusApp/ClientApp/src/components/Reportes.js
+++ FicusApp/ClientApp/src/components/Reportes.js
@@ -1,84 +1,87 @@
 ﻿import React, { useEffect, useState } from 'react';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
+
 import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 const options = {
-    maintainAspectRatio: false,
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
-        },
+  maintainAspectRatio: true,
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
     },
+  },
 };
 
 const Reportes = () => {
-    const [dataLine, setDataLine] = useState({});
-    const [environmentalReport, setEnvironmentalReport] = useState([]);
-    useEffect(() => {
-        const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-        const data = {
+  const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  const [environmentalReport, setEnvironmentalReport] = useState({});
+  useEffect(() => {
+    const getReport = async () => {
+      const response = await fetch(api / reporte / GetAnnualEnvironmentalReport / ${ 2023});
+      if (response.ok) {
+        const report = await response.json();
+        setEnvironmentalReport(
+          {
             labels: labels,
             datasets: [
-                {
-                    label: 'Kilogramos',
-                    data: [100, 50, 40, 75, 150, 200, 120], // Datos quemados para los trimestres
-                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                },
+              {
+                label: 'Kilogramos',
+                data: report,
+                backgroundColor: 'rgba(255, 44, 44, 1)',
+                borderColor: 'rgba(84, 74, 221, 0.5)'
+              },
             ],
-        };
-        const getReport = async () => {
-          const response = await fetch(`api/reporte/GetAnnualEnvironmentalReport/${2023}`);
-          if (response.ok) {
-            const report = await response.json();
-            console.log(report);
-          }
-        }
-        getReport();
-        setDataLine(data);
-    }, []);
 
-    return (
-        <div className="container vh-100">
-            <div className="row d-flex justify-content-center">
-                <div className="col-6">
-                    <h4 className="text-center">Reporte Huella Ambiental</h4>
-                </div>
-            </div>
-            <div className=  "row d-flex justify-content-center vh-50">
-                <div className= "col-6 bg-light d-flex justify-content-center">
-                    <div className="chart-container">
-                        <div className="chart-background" />
-                        {dataLine.datasets ? (
-                            <Line options={options} data={dataLine} />
-                        ) : (
-                            <p>Cargando datos...</p>
-                        )}
-                    </div>
-                </div> 
-            </div>
+          });
+      }
+    }
+    getReport();
+  }, []);
+
+  return (
+    <div className="container h-75">
+
+      <div className="row d-flex justify-content-center">
+        <div className="col-6">
+          <h4 className="text-center">Reporte Huella Ambiental</h4>
         </div>
-    );
+      </div>
+      <div className="row">
+        <div className="col p-0 mb-4">
+          <div className="bg-light m-0">
+            {
+              environmentalReport.datasets ?
+                <Line options={options} data={environmentalReport} />
+
+                :
+                <p>Cargando datos...</p>
+            }
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Reportes;
