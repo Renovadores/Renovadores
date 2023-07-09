260de049becb9f039a40b86654a6060b108e4857
kvn2912 <kevinjbr29@gmail.com>
2023-07-09 13:23:26 -0600

    Se cambió la paginación en clientes por una tabla

--- FicusApp/ClientApp/src/components/ClientList.js
+++ FicusApp/ClientApp/src/components/ClientList.js
@@ -1,20 +1,41 @@
 function ClientList(props) {
-
   return (
-    <div className="row m-2 mt-4">
+    <div className="row mx-2 px-3">
       {
         props.clients.length !== 0 ?
-          props.clients.map((client) => (
-            <div className="col-sm-6 col-md-3 mb-3" key={client.clienteId}>
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">{client.nombreEmpresa}</h5>
-                  <p className="card-text utf-8">Ordenes activas: </p>
-                  <button className="btn btn-primary" onClick={() => props.handler(client.clienteId)}>Ver cliente</button>
-                </div>
-              </div>
-            </div>
-          ))
+          <div className="card overflow-auto mb-5 px-0" style={{ height: "75vh" }}>
+            <table className="table table-hover border-success">
+              <thead className="bg-success">
+                <tr>
+                  <th scope="col" className="text-center text-light">
+                    ID
+                  </th>
+                  <th scope="col" className="text-center text-light">
+                    Nombre
+                  </th>
+                  <th scope="col" className="text-center text-light">
+                    Contacto
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody className="bg-light">
+                {
+                  props.clients.map((client) => (
+                    <tr key={client.clienteId}>
+                      <th scope="row" className="text-center">
+                        <a className="link" type="button" onClick={() => props.handler(client.clienteId)}>
+                          {client.clienteId}
+                        </a>
+                      </th>
+                      <td className="text-center">{client.nombreEmpresa}</td>
+                      <td className="text-center">{client.contacto}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
        :
           <h5 className="d-flex justify-content-center">No se encontraron clientes en la base de datos</h5>
       }
